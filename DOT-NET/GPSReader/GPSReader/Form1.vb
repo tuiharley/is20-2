4477518a146098a09a82029031ef1658bf08634b
tuiharley <tuiharley@gmail.com>
2015-04-16 01:34:11 +0700

    deleted

--- DOT-NET/GPSReader/GPSReader/Form1.vb
+++
@@ -1,9 +0,0 @@
-Public Class Form1
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Dim myX As New HighPrecisionTest
-
-        myX.Test()
-        txtResult.Text = myX.getMyTxt
-    End Sub
-End Class
