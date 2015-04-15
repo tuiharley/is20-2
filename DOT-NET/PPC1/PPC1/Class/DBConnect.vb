fdf1f82123fce4588009f263725a61d61182a5a8
tuiharley <tuiharley@gmail.com>
2015-04-16 01:33:45 +0700

    deleted

--- DOT-NET/PPC1/PPC1/Class/DBConnect.vb
+++
@@ -1,8 +0,0 @@
-Module DBConnect
-    Function getStrConnect() As String
-        Dim ConnStr As String
-        ConnStr = "Data Source=My Documents\VJTIMBER.sdf;Password=vj1596"
-        Return ConnStr
-    End Function
-
-End Module
